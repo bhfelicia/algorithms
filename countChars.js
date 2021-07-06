f335949a7541fc4fa1c02be578943139768351dc
Felicia Heiney <bhfelicia@gmail.com>
2021-07-06 17:48:34 -0400

    countChars algo

---
+++ countChars.js
@@ -0,0 +1,16 @@
+//write a function that takes a string and returns counts of each character in the string
+
+const countChars = (str) => {
+  str = str.toLowerCase();
+  const charCount = {};
+  str.split("").forEach((letter) => {
+    if (charCount[letter]) {
+      ++charCount[letter];
+    } else {
+      charCount[letter] = 1;
+    }
+  });
+  return charCount;
+};
+
+console.log(countChars("njirehuasfioeqajkapd"));
