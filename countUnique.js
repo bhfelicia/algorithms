b4b5a943e5049ccd687c158cf3c0cf2efb5327f4
Felicia Heiney <bhfelicia@gmail.com>
2021-07-08 12:29:11 -0400

    countUnique accepts sorted array and counts and returns unique values in array

---
+++ countUnique.js
@@ -0,0 +1,23 @@
+//write a function that accepts a sorted array and counts unique values in array
+
+function countUnique(arr) {
+  //alter array to only store unique values in array and take out
+
+  //pointer at beginning and index 1
+  if (arr.length < 1) return 0;
+  let pointOne = 0;
+  let secondPoint = 1;
+
+  while (secondPoint < arr.length) {
+    if (arr[pointOne] === arr[secondPoint]) {
+      secondPoint++;
+    } else {
+      pointOne++;
+      arr.splice(pointOne, 1, arr[secondPoint]);
+      console.log(arr);
+    }
+  }
+  return pointOne + 1;
+}
+
+console.log(countUnique([1, 1, 1, 1, 2]));
