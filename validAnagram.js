a70c009ff1b2e543eaab475531f2288a9747a052
Felicia Heiney <bhfelicia@gmail.com>
2021-07-08 09:32:29 -0400

    cleanup

--- validAnagram.js
+++ validAnagram.js
@@ -3,23 +3,13 @@ function validAnagram(str1, str2) {
   if (str1 === "" && str2 === "") return true;
   if (str1.length !== str2.length) return false;
   const freqCount1 = {};
-
   str1.split("").forEach((char) => {
     freqCount1[char] = (freqCount1[char] || 0) + 1;
   });
-
   for (let i = 0; i < str2.length; i++) {
     if (!freqCount1[str2[i]]) return false;
     else freqCount1[str2[i]] -= 1;
   }
-  // str2.split("").forEach((char) => {
-  //   if (!freqCount1[char]) return false;
-  //   else freqCount1[char] -= 1;
-  // });
-  // for (let key in freqCount1) {
-  //   if (!(key in freqCount2)) return false;
-  //   if (freqCount1[key] !== freqCount2[key]) return false;
-  // }
   return true;
 }
 
