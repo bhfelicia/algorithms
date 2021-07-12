f1a0f0eae0e5085dbf1ef961bf50b38efed93b2e
Felicia Heiney <bhfelicia@gmail.com>
2021-07-12 14:21:56 -0400

    bubble sort bubbles up largest number of an array of numbers until that array is sorted

---
+++ bubbleSort.js
@@ -0,0 +1,16 @@
+//Sorting
+
+//sorts arrays by bubbling up largest number until all sorted
+
+function bubbleSort(arr) {
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubbleSort([4, 38, 2, 27, 6, 7, 66, 9, 2]));
