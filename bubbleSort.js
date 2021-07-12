00900c249ecb36a2e2de8086ed40c564ebb0fcb1
Felicia Heiney <bhfelicia@gmail.com>
2021-07-12 14:41:45 -0400

    optimization of bubblesort in which we check if there are swaps at each outer loop iteration. if none, we break

--- bubbleSort.js
+++ bubbleSort.js
@@ -3,12 +3,16 @@
 //sorts arrays by bubbling up largest number until all sorted
 
 function bubbleSort(arr) {
+  let noSwaps;
   for (let i = arr.length; i > 0; i--) {
+    noSwaps = true;
     for (let j = 0; j < i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        noSwaps = false;
       }
     }
+    if (noSwaps) break;
   }
   return arr;
 }
