d1f8c3be43cdc2abd01c727d82ac65820188de4d
Felicia Heiney <bhfelicia@gmail.com>
2021-07-12 18:06:13 -0400

    implements merge sort

--- mergeSort.js
+++ mergeSort.js
@@ -23,15 +23,17 @@ function merge(arr1, arr2) {
     newArr.push(arr2[j]);
     j++;
   }
-  // if (i < arr1.length - 1) {
-  //   const remainingVals = arr1.slice(i + 1, arr1.length);
-  //   newArr.concat(remainingVals);
-  // }
-  // if (j < arr2.length - 1) {
-  //   const remaining = arr2.slice(j + 1, arr2.length);
-  //   newArr.concat(remaining);
-  // }
   return newArr;
 }
 
-console.log(merge([100, 200], [0, 4, 6, 7, 8]));
+//mergeSort breaks the array into halves until you have arrays that are empty or have 1 element each
+//once have smaller sorted arrays, merge those w other sorted arrays until back at full length of input array
+//return merged/sorted array
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+  const middle = Math.floor(arr.length / 2);
+  const left = mergeSort(arr.slice(0, middle));
+  const right = mergeSort(arr.slice(middle));
+  return merge(left, right);
+}
+console.log(mergeSort([10, 24, 76, 73, 72, 1, 9]));
