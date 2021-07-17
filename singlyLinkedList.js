c991da1341404a2b7547606e29b46f769217acdc
Felicia Heiney <bhfelicia@gmail.com>
2021-07-17 18:26:28 -0400

    singly linked list has reverse method which reverses our linked list

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -80,4 +80,56 @@ class SinglyLinkedList {
     }
     return false;
   }
+  insert(idx, val) {
+    if (idx < 0 || idx > this.length) return false;
+    if (idx === this.length) return !!this.push(val);
+    if (idx === 0) return !!this.unshift(val);
+    const newNode = new Node(val);
+    const previous = this.get(idx - 1);
+    newNode.next = previous.next;
+    previous.next = newNode;
+    this.length++;
+    return true;
+  }
+  remove(idx) {
+    if (idx < 0 || idx >= this.length) return undefined;
+    if (idx === this.length - 1) return this.pop();
+    if (idx === 0) return this.shift();
+    const prev = this.get(idx - 1);
+    const removed = prev.next;
+    prev.next = removed.next;
+    --this.length;
+    return removed;
+  }
+  reverse() {
+    let node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+    let next;
+    let previous = null;
+    while (node) {
+      next = node.next;
+      node.next = previous;
+      previous = node;
+      node = next;
+    }
+    return this;
+  }
+  print() {
+    const arr = [];
+    let current = this.head;
+    while (current) {
+      arr.push(current.val);
+      current = current.next;
+    }
+    console.log(arr);
+  }
 }
+const list = new SinglyLinkedList();
+list.push(100);
+list.push(200);
+list.push(300);
+list.push(400);
+list.print();
+list.reverse();
+list.print();
