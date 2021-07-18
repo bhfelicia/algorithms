5b49722b183d26974ad2d963cc1e06b4fb3bec0e
Felicia Heiney <bhfelicia@gmail.com>
2021-07-18 12:35:26 -0400

    shift method on doubly linked list removes head

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -39,4 +39,18 @@ class DoublyLinkedList {
     --this.length;
     return oldTail;
   }
+  shift() {
+    if (!this.head) return undefined;
+    let oldHead = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = oldHead.next;
+      this.head.prev = null;
+      oldHead.next = null;
+    }
+    --this.length;
+    return oldHead;
+  }
 }
