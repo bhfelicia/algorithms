5a8adadf56ab4de07555d64f9005dcaada927314
Felicia Heiney <bhfelicia@gmail.com>
2021-07-18 18:41:54 -0400

    implements stack class using singly linked list

---
+++ stack.js
@@ -0,0 +1,26 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      const oldFirst = this.first;
+      this.first = newNode;
+      this.first.next = oldFirst;
+    }
+    return ++this.size;
+  }
+}
