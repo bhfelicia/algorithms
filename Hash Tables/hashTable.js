51683718c61f6de824338d9db418b9d4384e8eff
Felicia Heiney <bhfelicia@gmail.com>
2021-08-30 17:47:08 -0400

    adds hash table class with hash function

--- Hash Tables/hashTable.js
+++ Hash Tables/hashTable.js
@@ -27,3 +27,66 @@ function hash2(key, arrLen) {
   return total;
 }
 //how do we handle collisions?
+//can use separate chaining and linear probing
+//with separate chaining, at each idx in array we store values using a more sophisticated data structure to allow us to store multiple vals at same index (eg array or linked list)
+//eg if darkblue and salmon both evaluate to 4, store them like [["darkblue", '#00008b], ["salmon", "#fa8072"]] - 2 arrays within an array
+//linear probing is a strategy such that when we find a collision, we search through the array to find the next empty slot so as to keep each index at only one key-value pair
+//hash table class
+
+class HashTable {
+  constructor(size = 53) {
+    this.keyMap = new Array(size);
+  }
+  _hash(key) {
+    let total = 0;
+    let prime = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * prime + value) % this.keyMap.length;
+    }
+    return total;
+  }
+  set(key, value) {
+    const num = this._hash(key);
+    if (!this.keyMap[num]) {
+      this.keyMap[num] = [];
+    }
+    this.keyMap[num].push([key, value]);
+  }
+  get(key) {
+    const num = this._hash(key);
+    if (this.keyMap[num]) {
+      const nested = this.keyMap[num].filter((arr) => {
+        arr.includes(key);
+      });
+      return nested[0][1];
+    }
+    return undefined;
+  }
+  keys(curr = this, array = []) {
+    // if(Array.isArray(curr[0])) {
+    //   array.push(this.keys(curr[]))
+    // } else {
+    //   if(this.get(curr[0]))
+    // }
+  }
+  values() {
+    const valuesArr = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          if (!valuesArr.includes(this.keyMap[i][j][1]))
+            valuesArr.push(this.keyMap[i][j][1]);
+        }
+      }
+    }
+    return valuesArr;
+  }
+}
+const newHash = new HashTable(41);
+newHash.set("hello", "goodbye");
+newHash.set("pb", "jam");
+newHash.set("cookies", "cream");
+newHash.set("you", "me");
+console.log(newHash.values());
